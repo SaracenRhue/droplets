6370aede83a337f3b971e069c299ea2afd17ceaa
SaracenRhue <77064702+SaracenRhue@users.noreply.github.com>
2024-01-26 09:00:11 +0100

    Update containerBuilder.sh

--- containerBuilder.sh
+++ containerBuilder.sh
@@ -1,16 +1,14 @@
 #!/usr/bin/env bash
 
-apt update
-apt install git tmux htop -y
+apt update && apt upgrade -y
+apt install git tmux htop python3 python3-pip python-is-python3 nodejs npm wget -y
+curl -fsSL https://tailscale.com/install.sh | sh
 
-curl -fsSL https://get.docker.com -o get-docker.sh
-sh get-docker.sh
+curl -fsSL https://get.docker.com | sh 
 usermod -aG docker $USER
 apt install docker-compose -y
-systemctl enable docker
-systemctl start docker
+systemctl enable docker && systemctl start docker
 source ~/.bashrc
-rm -fr get-docker.sh
 
 docker buildx create --name mybuilder
 docker buildx use mybuilder
